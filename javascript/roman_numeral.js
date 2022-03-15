eb63ac8f23d1e34d0ff4732205263272267a155f
Sarah Nosal <nosalsm@gmail.com>
2022-03-15 14:19:52 -0500

    completed

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,36 @@
 function romanNumeral(string) {
   // type your code here
+  const romans = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M:1000
+  }
+  let total = 0
+  let idx = 0
+
+  while (idx < string.length) {
+    const twoChar = string[idx] + string[idx + 1] || ''
+
+    if (romans[twoChar] !== undefined) {
+      total += romans[twoChar]
+      idx += 2
+    } else {
+      total += romans[string[idx]]
+      ++idx
+    }
+  }
+
+  return total
 }
 
 if (require.main === module) {
--- ruby/roman_numeral.rb
+++ ruby/roman_numeral.rb
@@ -1,5 +1,36 @@
 def roman_numeral(string)
   # type your code in here
+  romans = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000
+  }
+  total = 0
+  idx = 0
+
+  while idx < string.length
+    twoChar = (string[idx] + (string[idx + 1] || '')).to_sym
+
+    if !romans[twoChar].nil?
+      total += romans[twoChar]
+      idx += 2
+    else
+      total += romans[string[idx].to_sym]
+      idx += 1
+    end
+  end
+
+  total
 end
 
 if __FILE__ == $PROGRAM_NAME
